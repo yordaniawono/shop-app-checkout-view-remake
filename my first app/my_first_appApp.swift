64ba54030ed59ef7fb874944ae03f9439e4e6f8b
yordani <63028285+yordaniawono@users.noreply.github.com>
2026-02-05 07:32:44 -0500

    Initial Commit

---
+++ my first app/my_first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_first_appApp.swift
+//  my first app
+//
+//  Created by Yordani Awono on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct my_first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ my first appTests/my_first_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  my_first_appTests.swift
+//  my first appTests
+//
+//  Created by Yordani Awono on 2/5/26.
+//
+
+import Testing
+@testable import my_first_app
+
+struct my_first_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
